c309732e2fec67d8db3c15231e4b88909f4bff24
Gary <xzblaze@hotmail.com>
2015-05-22 21:53:37 +0800

    Initial Commit

---
+++ HongKongSchools/HongKongSchools.Shared/App.xaml.cs
@@ -0,0 +1,65 @@
+﻿using HongKongSchools.Services.NavigationService;
+using Microsoft.Practices.Prism.Mvvm;
+using SimpleInjector;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=234227
+
+namespace HongKongSchools
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public sealed partial class App : MvvmAppBase
+    {
+#if WINDOWS_PHONE_APP
+        private TransitionCollection transitions;
+#endif
+
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public static readonly Container _container = new Container();
+
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override async Task OnInitializeAsync(IActivatedEventArgs args)
+        {
+            _container.RegisterSingle(NavigationService);
+            _container.Register<INavigationService, NavigationService>();
+            return;
+        }
+
+        protected override object Resolve(Type type)
+        {
+            return _container.GetInstance(type);
+        }
+
+        protected override Task OnLaunchApplicationAsync(LaunchActivatedEventArgs e)
+        {
+            NavigationService.Navigate(Experiences.Main.ToString(), null);
+            return Task.FromResult<object>(null);
+        }
+    }
+}
---
+++ HongKongSchools/HongKongSchools.Shared/Controls/PageBase.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Practices.Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Controls;
+
+namespace HongKongSchools.Controls
+{
+    public abstract partial class PageBase : Page, IView { }
+}
